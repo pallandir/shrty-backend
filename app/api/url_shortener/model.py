443ea2539d1cea417c2536c8e1f8e1e9bb2001f9
pallandir <pallandir2@gmail.com>
2025-02-23 23:59:49 +0800

    Update requirements and ORM model

--- app/api/url_shortener/model.py
+++ app/api/url_shortener/model.py
@@ -4,9 +4,11 @@ from app.database import Base
 
 
 class URL(Base):
+    __tablename__ = "urls"
+
     id: Mapped[int] = mapped_column(primary_key=True)
     short_url: Mapped[str] = mapped_column(unique=True)
-    mapped_url = Mapped[str] = mapped_column()
+    mapped_url: Mapped[str] = mapped_column()
     visited: Mapped[int] = mapped_column(nullable=True)
     last_visit: Mapped[date] = mapped_column(nullable=True)
     last_update: Mapped[date] = mapped_column(nullable=True)
